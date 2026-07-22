009c9846814f96b6ee8836be67f70f8f6a8d286b
jarvis <jarvis.selvam@valuemomentum.com>
2026-07-22 06:44:35 +0000

    Revert Changes

--- step-definition/Homepage.steps.ts
+++ step-definition/Homepage.steps.ts
@@ -10,5 +10,6 @@ import { HomePage } from '../pages/HomePage';
   });
 
   When('I sort the products by {string}', async function (this: CustomWorld, sortOption: string) {
-    const homePage1 = new HomePage(this.page!);
+    const homePage = new HomePage(this.page!);
+    await homePage.sortProducts(sortOption);
   });
